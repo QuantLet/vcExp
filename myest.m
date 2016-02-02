96db81ec97ae75072c18d5cfc507c0367341e744
tds18 <tds18@users.noreply.github.com>
2016-02-02 23:26:48 +0100

    Update myest.m

--- myest.m
+++ myest.m
@@ -13,7 +13,9 @@ bx = b;
 b0 = zeros(2*c-1,1);
 z  = [X(:,1),X(:,2),X(:,3), X(:,2).*(u-ug), X(:,3).*(u-ug)];
 k  = 0;
-%%
+
+% Iterate to obatain the estimator of convergence
+
 while((norm(abs(b0-bx))>1e-5)&&(k <=1000))   
 
     eps = y - z*bx;
