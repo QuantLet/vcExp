231b91ec7aaf716fa80831afb2261af6b5b5c921
tds18 <tds18@users.noreply.github.com>
2016-02-05 09:44:35 +0100

    Update mydgp.m

--- VCCARE/mydgp.m
+++ VCCARE/mydgp.m
@@ -7,19 +7,19 @@ Y(2) = 1;
 % generate initial value of Y  
 if C == 1                               % Example 1
     for t = 1 : (T + 100)
-        uu     = Y(t + 1);
-        a1     = 0.138 + (0.316 + 0.982*uu)*exp(-3.89*uu^2);
-        a2     = -0.437 - (0.659 + 0.126*uu)*exp(-3.89*uu^2);
-        eps    = normrnd(0, 0.2);
+        uu       = Y(t + 1);
+        a1       = 0.138 + (0.316 + 0.982*uu)*exp(-3.89*uu^2);
+        a2       = -0.437 - (0.659 + 0.126*uu)*exp(-3.89*uu^2);
+        eps      = normrnd(0, 0.2);
         Y(t + 2) = Y(t + 1)*a1 + Y(t)*a2 + 2*a1*Y(t + 1)*eps*(uu >= 0);
     end
     a = Y(103 : T + 102);                               % Y(t):delete the first 100 values
     b = [ones(T, 1),Y(102 : T + 101),Y(101 : T + 100)];      % X(t) = [1, Y(t-1), Y(t-2)]
 elseif C == 2                            % Example 2
     for t = 1 : (T + 100)
-        uu     = Y(t);
-        a1     = 0.4*(uu <= 1) - 0.8*(uu > 1);
-        a2     = -0.6*(uu <= 1) - 0.2*(uu > 1);
+        uu       = Y(t);
+        a1       = 0.4*(uu <= 1) - 0.8*(uu > 1);
+        a2       = -0.6*(uu <= 1) - 0.2*(uu > 1);
         Y(t + 2) = Y(t + 1)*a1 + Y(t)*a2 + normrnd(0, 1);
     end
     a = Y(103 : T + 102);                               % Y(t):delete the first 100 values
