da941d5f1c093e2c3c5f6265b9c009cfcca6def0
tds18 <tds18@users.noreply.github.com>
2016-02-15 16:47:45 +0800

    Update main.m

--- VCCARE/main.m
+++ VCCARE/main.m
@@ -62,6 +62,30 @@ elseif C == 3                             % Example 3
     b = [ones(T, 1),Y(102 : T + 101), Y(101 : T + 100), uu(101 : T + 100)];      % X(t) = [1, Y(t-1), Y(t-2),U]
 end
 
+function[b]=myest(y, X, u, ug, T)              % Obtaining initial estimators
+
+% Variable setting 
+bd02   = 1.06*std(y)*T^(-0.2);
+[~, c] = size(X);
+b      = zeros(2*c - 1, length(ug));
+S      = zeros(2*c - 1, 2*c - 1);
+R      = zeros(2*c - 1, 1);
+ 
+% obatain the initial value of estimators 
+for i = 1:length(ug)
+    z = [X(:, 1), X(:, 2), X(:, 3), X(:, 2).*(u - ug(i)), X(:, 3).*(u - ug(i))];
+    for t = 1:T
+        kernel01 = 1/sqrt(2*pi)*exp(-0.5*((u(t) - ug(i))/bd02)^2);
+        s        = kernel01*z(t, :)'*z(t, :);
+        S        = s + S;
+        r        = kernel01*z(t, :)'*y(t);
+        R        = r + R;
+    end
+    b(:, i) = S\R;
+end
+
+return(b)
+
 % monte carlo simulations for M times
 for n = 1:length(TT)
     T = TT(n);
